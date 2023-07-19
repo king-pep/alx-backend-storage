c27bd75439580754304e1a08f81aaeed538cc690
Abel Ben <abelben59@gmail.com>
2023-07-19 17:56:36 +0000

    0x02

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,51 +1,32 @@
-import requests
-import time
+#!/usr/bin/env python3
+""" Redis Module """
+
 from functools import wraps
+import redis
+import requests
+from typing import Callable
 
-# Decorator function for caching and tracking
-def cache_and_track(func):
-    cache = {}  # Cache dictionary to store results
-    counts = {}  # Dictionary to store access counts
+redis_ = redis.Redis()
 
-    @wraps(func)
-    def wrapper(url):
-        # Check if URL is in cache and not expired
-        if url in cache and time.time() < cache[url]['expiration']:
-            cache[url]['count'] += 1  # Increment access count
-            return cache[url]['content']
 
-        # Retrieve the HTML content using requests
-        response = requests.get(url)
-        content = response.text
-
-        # Update cache with the new content and expiration time
-        cache[url] = {
-            'content': content,
-            'expiration': time.time() + 10  # Cache expires after 10 seconds
-        }
-
-        # Initialize access count if URL is accessed for the first time
-        if url not in counts:
-            counts[url] = 0
-        counts[url] += 1  # Increment access count
-
-        # Print the access count
-        print(f"Access count for {url}: {counts[url]}")
-
-        return content
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ Wrapper for decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
     return wrapper
 
-# Decorate the get_page function with the cache_and_track decorator
-@cache_and_track
-def get_page(url):
-    return requests.get(url).text
 
-
-# Example usage
-print(get_page("http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"))
-time.sleep(5)
-print(get_page("http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"))
-time.sleep(10)
-print(get_page("http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"))
-
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain the HTML content of a  URL """
+    req = requests.get(url)
+    return req.text
